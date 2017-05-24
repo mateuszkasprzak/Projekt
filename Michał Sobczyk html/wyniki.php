0329ed332b3fb7307486f088d7b73e5ab261c8e8
S0czek <mrsoczek@gmail.com>
2017-05-25 00:56:56 +0200

    szkielet wyszukiwarki

---
+++ Michał Sobczyk html/wyniki.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Wyniki wyszukiwania prowadzacych w bazie prowadzacy</title>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <h1>Wyniki wyszukiwania prowadzacych w bazie prowadzacy</h1>
+    <?php
+      $metoda = $_POST['metoda'];
+      $wyrazenie = $_POST['wyrazenie'];
+      $wyrazenie = trim($wyrazenie);
+      if (!$metoda || !$wyrazenie)
+      {
+        echo 'Brak parametrów wyszukiwania, wróæ do poprzednej strony i spóbuj ponownie!';
+        exit;
+      }
+      if (!get_magic_quotes_gpc())
+      {
+        $metoda = addslashes($metoda);
+        $wyrazenie = addslashes($wyrazenie);
+      }
+      @ $db = new mysqli('www.mysql.agh.edu.pl','wliszka','FYc3MmSSefkv7CXy','wliszka');
+      
+      if (mysqli_connect_errno())
+      {
+        echo 'Po³¹czenie z baz¹ nie powiod³o siê. Spóbuj ponownie';
+        exit;
+      }
+      $db->query('SET NAMES utf8');
+      $db->query('SET CHARACTER_SET utf8_unicode_ci');
+      $zapytanie = "select * from prowadzacy where ".$metoda. " like '%".$wyrazenie."%'";
+      $wynik = $db->query($zapytanie);
+      $ile_znaleziono = $wynik->num_rows;
+      echo '<p> Liczba znalezionych prowadzacych: '.$ile_znaleziono.'</p>';
+      for ($i=0;$i<$ile_znaleziono;$i++)
+      {
+        $wiersz = $wynik->fetch_assoc();
+        echo '<p><b>'.($i+1).'. Nazwisko: '.stripslashes($wiersz['nazwisko']).'</b><br />';
+        echo 'Imie: '.stripslashes($wiersz['imie']).'<br />';
+	echo 'Tytul: '.stripslashes($wiersz['tytul']).'<br />';
+	echo 'Strona: '.stripslashes($wiersz['strona']).'<br />';
+        echo 'Siedziba: '.stripslashes($wiersz['Siedziba']).'<br />';
+        echo 'Mail: '.stripslashes($wiersz['mail']).'</p><br />';
+      }
+      $wynik->free();
+      $db->close();
+    ?> 
+  </body>
+</html>
